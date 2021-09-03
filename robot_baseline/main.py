2a312233c21118c161b2f577215c78015c00808a
shiuv <cbc107023@nptu.edu.tw>
2021-09-03 16:21:58 +0800

    update

--- robot_baseline/main.py
+++ robot_baseline/main.py
@@ -1,5 +1,3 @@
-import gym
-
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common import make_vec_env
 from stable_baselines import PPO2
@@ -24,29 +22,3 @@ while True:
     obs, rewards, dones, info = env.step(action)
     # env.render()
 
-
-import gym
-
-# from stable_baselines3 import PPO
-# from stable_baselines3.common.env_util import make_vec_env
-#
-# # Parallel environments
-# env = make_vec_env('robot-v0', n_envs=1)
-#
-# model = PPO("MlpPolicy", env, verbose=1)
-# model.learn(total_timesteps=1024,n_eval_episodes=3)
-# model.save("ppo2_robot")
-#
-# del model # remove to demonstrate saving and loading
-#
-# model = PPO.load("ppo2_robot")
-#
-# obs = env.reset()
-# while True:
-#     action, _states = model.predict(obs)
-#     obs, rewards, dones, info = env.step(action)
-#     # env.render()
-
-
-# model = PPO2.load("ppo2_robot")
-# print(PPO2.get_parameters(model))
