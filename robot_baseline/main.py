598ed419e9b2ed10455879c4a4db2a098617d5a6
shiuv <shiuvic@gmail.com>
2023-05-03 22:32:06 +0800

    update to stable baselines3

--- robot_baseline/main.py
+++ robot_baseline/main.py
@@ -1,24 +1,25 @@
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common import make_vec_env
-from stable_baselines import PPO2
+##PPO
+
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3 import PPO
 from robot_baseline import robot
 
 # multiprocess environment
 
 env = make_vec_env('robot-v0', n_envs=1)
 
-model = PPO2(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=30000)
-model.save("ppo2_robot")
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=124805)
+# model.save("ppo2_robot")
 
-del model # remove to demonstrate saving and loading
-
-model = PPO2.load("ppo2_robot")
-
-# Enjoy trained agent
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    # env.render()
+# del model # remove to demonstrate saving and loading
+#
+# model = PPO2.load("ppo2_robot")
+#
+# # Enjoy trained agent
+# obs = env.reset()
+# while True:
+#     action, _states = model.predict(obs)
+#     obs, rewards, dones, info = env.step(action)
+#     env.render()
 
